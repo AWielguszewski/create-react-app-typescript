2669030859627b2ecf55f5dd6fcb85b73219da7f
AlanRedvike <alan.wielguszewski@redvike.com>
2018-04-23 11:55:30 +0200

    resolving problem on OSX

--- bin/pretty-typed-react-starter.js
+++ bin/pretty-typed-react-starter.js
@@ -6,17 +6,17 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-'use strict';
+'use strict'
+console.log('gowno')
 
-const spawn = require('react-dev-utils/crossSpawn');
-const args = process.argv.slice(2);
+const spawn = require('react-dev-utils/crossSpawn')
+const args = process.argv.slice(2)
 
 const scriptIndex = args.findIndex(
-  x => x === 'build' || x === 'eject' || x === 'start' 
-);
-const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
-const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
-
+  x => x === 'build' || x === 'eject' || x === 'start'
+)
+const script = scriptIndex === -1 ? args[0] : args[scriptIndex]
+const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : []
 switch (script) {
   case 'build':
   case 'eject':
@@ -27,28 +27,28 @@ switch (script) {
         .concat(require.resolve('../scripts/' + script))
         .concat(args.slice(scriptIndex + 1)),
       { stdio: 'inherit' }
-    );
+    )
     if (result.signal) {
       if (result.signal === 'SIGKILL') {
         console.log(
           'The build failed because the process exited too early. ' +
             'This probably means the system ran out of memory or someone called ' +
             '`kill -9` on the process.'
-        );
+        )
       } else if (result.signal === 'SIGTERM') {
         console.log(
           'The build failed because the process exited too early. ' +
             'Someone might have called `kill` or `killall`, or the system could ' +
             'be shutting down.'
-        );
+        )
       }
-      process.exit(1);
+      process.exit(1)
     }
-    process.exit(result.status);
-    break;
+    process.exit(result.status)
+    break
   }
   default:
-    console.log('Unknown script "' + script + '".');
-    console.log('Perhaps you need to update pretty-typed-react-starter?');
-    break;
+    console.log('Unknown script "' + script + '".')
+    console.log('Perhaps you need to update pretty-typed-react-starter?')
+    break
 }
